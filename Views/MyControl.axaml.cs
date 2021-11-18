0a9576ad88b9fc3d612cea754104bc245b060b3e
GMIKE <roman.soloweow333@ya.ru>
2021-11-18 03:04:29 +0300

    Initial commit

---
+++ Views/MyControl.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using LayoutTransformControlWithStyleBug.ViewModels;
+
+namespace LayoutTransformControlWithStyleBug.Views
+{
+    public class MyControl : ReactiveUserControl<ViewModelBase>
+    {
+        #region IsRotatedProperty
+
+        public static readonly AvaloniaProperty<bool> IsRotatedProperty =
+            AvaloniaProperty.Register<MyControl, bool>(nameof(IsRotated));
+
+        public bool IsRotated
+        {
+            get => this.GetValue<bool>(IsRotatedProperty);
+            set => SetValue(IsRotatedProperty, value);
+        }
+
+        #endregion IsRotatedProperty
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
